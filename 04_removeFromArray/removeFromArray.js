bf2206b7f11f32e67f8909ef56e054382f5bdead
Ronald <ronaldreyjr061201@gmail.com>
2023-02-18 21:50:02 +0800

    My solutions

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let copyArr = args[0];
+    for(let i = 1; i < args.length; i++){
+        if(copyArr.includes(args[i])){
+            let leftSide = copyArr.slice(0,copyArr.indexOf(args[i]));
+            let rightSide = copyArr.slice(copyArr.indexOf(args[i]) + 1);
+            copyArr = leftSide.concat(rightSide);
+        }
+    }
+    return copyArr;
 };
 
 // Do not edit below this line
